b95b2b2437b70df42c866173ef8b66dda524d2e7
Muhammad Bin Naeem <mbinnaeem20@gmail.com>
2021-01-11 18:00:57 +0500

    checkout command

---
+++ app/Console/Commands/Checkout.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Checkout extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'checkout';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Checkout';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // create new cart
+        $cart = new Cart;
+        $cart->save();
+        $this->info("Empty Cart.");
+    }
+}
